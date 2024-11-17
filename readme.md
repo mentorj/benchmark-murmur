c73e9d11a046f690908b9c205b7296b3983c3974
jerome moliere <jerome@javaxpert.com>
2024-11-17 16:11:44 +0100

    adds readme & some new benchmarks

---
+++ readme.md
@@ -0,0 +1,18 @@
+# Benchmark Murmur algorithms & implementations
+
+
+## Introduction
+
+This repository compares different hashing implementations from different editors:
++ Apache Commons Coded
++ Guava
++ Another lib fetched from github
+
+
+## Benchmark
+This repository uses the OpenJDK"s JMH tool to provide quite real numbers. This tool helps to avoid some pitfalls while coding some benchmarks in Java.
+
+## How to use it
+
+Just run the only test class with Maven or your IDE.
+
--- src/test/java/com/javaxpert/benchmarks/murmur/HashingMurmurBenchmark.java
+++ src/test/java/com/javaxpert/benchmarks/murmur/HashingMurmurBenchmark.java
@@ -1,5 +1,8 @@
 package com.javaxpert.benchmarks.murmur;
 
+import com.google.common.hash.Hashing;
+import com.sangupta.murmur.Murmur2;
+import com.sangupta.murmur.Murmur3;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
 
@@ -39,10 +42,34 @@ public class HashingMurmurBenchmark {
     public void  longHashWithMurmur3(MyStringState state, Blackhole bh){
         bh.consume(MurmurHash3.hash64(state.targetString.getBytes()));
     }
+    @Benchmark
+    public void  hash128WithApacheCommonsMurmur3(MyStringState state, Blackhole bh){
+        bh.consume(MurmurHash3.hash128x64(state.targetString.getBytes()));
+    }
 
     @Benchmark
     public void  hashWithMurmur3(MyStringState state, Blackhole bh){
         bh.consume(MurmurHash3.hash32(state.targetString));
+    }
+
+    @Benchmark
+    public  void hash32WithGuava(MyStringState  state,Blackhole bh){
+        bh.consume(Hashing.murmur3_32_fixed(101).hashBytes(state.targetString.getBytes()));
+    }
+
+    @Benchmark
+    public  void hash128WithGuava(MyStringState  state,Blackhole bh){
+        bh.consume(Hashing.murmur3_128(10).hashBytes(state.targetString.getBytes()));
+    }
+
+    @Benchmark
+    public void  hashWithSangupta(MyStringState state, Blackhole bh){
+        bh.consume(Murmur2.hash(state.targetString.getBytes(),state.targetString.length(),0x7f3a21eal));
+    }
+
+    @Benchmark
+    public void  hash128WithSangupta(MyStringState state, Blackhole bh){
+        bh.consume(Murmur3.hash_x64_128(state.targetString.getBytes(),state.targetString.length(),0x7f3a21eal));
     }
 
     public static void main(String[] args) throws RunnerException {
